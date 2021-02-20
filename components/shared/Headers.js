423683ebe0c4e82e553d62070b440d567db849ba
smadzelonka <smadzelonka@gmail.com>
2021-02-19 22:11:47 -0800

    working again and new header

--- components/shared/Headers.js
+++ components/shared/Headers.js
@@ -11,69 +11,76 @@ export default class Header extends Component {
     const { activeItem } = this.state;
 
     return (
-      <Menu stackable>
-        <Menu.Item>
-          <img src="/images/logo.png" />
-        </Menu.Item>
+      <Menu
+        stackable
+        // color=""
+        className="port-navbar port-default absolute"
+      >
+        <Menu.Menu position="left">
+          <Menu.Item>
+            <img src="/images/logo.png" />
+          </Menu.Item>
 
-        <Menu.Item
-          name="home"
-          active={activeItem === "home"}
-          onClick={this.handleItemClick}
-        >
-          <Link href="/">
-            <a className="headerItem">Home</a>
-          </Link>
-        </Menu.Item>
+          <Menu.Item
+            name="home"
+            active={activeItem === "home"}
+            onClick={this.handleItemClick}
+          >
+            <Link href="/">
+              <a className="headerItem">Home</a>
+            </Link>
+          </Menu.Item>
 
-        <Menu.Item
-          name="about"
-          active={activeItem === "about"}
-          onClick={this.handleItemClick}
-        >
-          <Link href="/about">
-            <a className="headerItem">About</a>
-          </Link>
-        </Menu.Item>
+          <Menu.Item
+            name="about"
+            active={activeItem === "about"}
+            onClick={this.handleItemClick}
+          >
+            <Link href="/about">
+              <a className="headerItem">About</a>
+            </Link>
+          </Menu.Item>
 
-        <Menu.Item
-          name="portfolios"
-          active={activeItem === "portfolios"}
-          onClick={this.handleItemClick}
-        >
-          <Link href="/portfolios">
-            <a className="headerItem">Portfolio</a>
-          </Link>
-        </Menu.Item>
+          <Menu.Item
+            name="portfolios"
+            active={activeItem === "portfolios"}
+            onClick={this.handleItemClick}
+          >
+            <Link href="/portfolios">
+              <a className="headerItem">Portfolio</a>
+            </Link>
+          </Menu.Item>
 
-        <Menu.Item
-          name="blog"
-          active={activeItem === "blog"}
-          onClick={this.handleItemClick}
-        >
-          <Link href="/blog">
-            <a className="headerItem">Blog</a>
-          </Link>
-        </Menu.Item>
+          <Menu.Item
+            name="blog"
+            active={activeItem === "blog"}
+            onClick={this.handleItemClick}
+          >
+            <Link href="/blog">
+              <a className="headerItem">Blog</a>
+            </Link>
+          </Menu.Item>
+        </Menu.Menu>
 
-        <Menu.Item
-          name="cv"
-          position="right"
-          active={activeItem === "cv"}
-          onClick={this.handleItemClick}
-        >
-          <Link href="/cv">
-            <a className="headerItem">CV</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item
-          // position="right"
-          name="sign-in"
-          active={activeItem === "sign-in"}
-          onClick={this.handleItemClick}
-        >
-          Sign-in
-        </Menu.Item>
+        <Menu.Menu position="right">
+          <Menu.Item
+            name="cv"
+            position="right"
+            active={activeItem === "cv"}
+            onClick={this.handleItemClick}
+          >
+            <Link href="/cv">
+              <a className="headerItem">CV</a>
+            </Link>
+          </Menu.Item>
+          <Menu.Item
+            name="sign-in"
+            active={activeItem === "sign-in"}
+            onClick={this.handleItemClick}
+          >
+            Sign-in
+          </Menu.Item>
+        </Menu.Menu>
       </Menu>
     );
   }
