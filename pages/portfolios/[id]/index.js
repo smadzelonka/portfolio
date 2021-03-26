e2463a2e43c37d4b85a093cd3d395f62f4f8067c
smadzelonka <smadzelonka@gmail.com>
2021-03-25 18:45:29 -0700

    remove non website button

--- pages/portfolios/[id]/index.js
+++ pages/portfolios/[id]/index.js
@@ -57,15 +57,19 @@ const Portfolio = ({ portfolio }) => {
                       Visit Site
                     </a>
                   </p>
-                  <p className="lead">
-                    <a
-                      href={portfolio.website}
-                      target="_"
-                      class="btn btn-lg btn-secondary"
-                    >
-                      Visit Repo
-                    </a>
-                  </p>
+                  {portfolio.website ? (
+                    <p className="lead">
+                      <a
+                        href={portfolio.website}
+                        target="_"
+                        class="btn btn-lg btn-secondary"
+                      >
+                        Visit Repo
+                      </a>
+                    </p>
+                  ) : (
+                    <></>
+                  )}
                 </>
               )}
             </main>
